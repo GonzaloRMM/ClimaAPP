2cd87f7d287811bf195a4d69aa3ecd8de594be66
DIR\gonzalo.ruiz.de.mier <gonrmm@gmail.com>
2021-01-11 17:26:47 +0100

    pasar de utc a date

--- app/src/main/java/com/example/clima/Adaptardor.java
+++ app/src/main/java/com/example/clima/Adaptardor.java
@@ -1,6 +1,7 @@
 package com.example.clima;
 
 import android.app.Activity;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,10 @@ import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class Adaptardor extends ArrayAdapter {
     private Activity context;
@@ -25,12 +30,11 @@ public class Adaptardor extends ArrayAdapter {
         LayoutInflater inflater = context.getLayoutInflater();
 
         View item = inflater.inflate(R.layout.list_view,null);
-        long date = System.currentTimeMillis();
 
-        SimpleDateFormat actualDate = new SimpleDateFormat("MMM dd");
-        String dateString = actualDate.format(date);
- 
-        ((TextView) item.findViewById(R.id.fecha)).setText("Fecha: "+datos.get(position).get(0));
+        long UTC_TIMEZONE=Long.parseLong(datos.get(position).get(0));
+        String OUTPUT_DATE_FORMATE="dd/MM/yyyy";
+
+        ((TextView) item.findViewById(R.id.fecha)).setText("Fecha: "+getDateFromUTCTimestamp(UTC_TIMEZONE,OUTPUT_DATE_FORMATE));
         ((TextView) item.findViewById(R.id.temperatura)).setText(datos.get(position).get(2));
         ((TextView) item.findViewById(R.id.temperaturaMax)).setText(datos.get(position).get(3));
         ((TextView) item.findViewById(R.id.temperaturaMinima)).setText(datos.get(position).get(4));
@@ -49,5 +53,26 @@ public class Adaptardor extends ArrayAdapter {
                 break;
         }
         return item;
+    }
+
+    private String getDateFromUTCTimestamp(long utc_timezone, String output_date_formate) {
+        String date = null;
+        try {
+            Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+            cal.setTimeInMillis(utc_timezone * 1000L);
+            date = DateFormat.format(output_date_formate, cal.getTimeInMillis()).toString();
+
+            SimpleDateFormat formatter = new SimpleDateFormat(output_date_formate);
+            formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+            Date value = formatter.parse(date);
+
+            SimpleDateFormat dateFormatter = new SimpleDateFormat(output_date_formate);
+            dateFormatter.setTimeZone(TimeZone.getDefault());
+            date = dateFormatter.format(value);
+            return date;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return date;
     }
 }
